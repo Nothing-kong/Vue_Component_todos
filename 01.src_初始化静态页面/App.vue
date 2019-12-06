5292503c6a29e2b5fc31d9d9a6c76a4f2fb3fa7c
KLong <15110232612@163.com>
2019-12-06 18:46:34 +0800

    11

---
+++ 01.src_初始化静态页面/App.vue
@@ -0,0 +1,46 @@
+<template>
+<div class="todo-container">
+  <div class="todo-wrap">
+     <Header/>
+     <List :todos='todos'/>
+     <Footer/> 
+  </div>
+</div>
+</template>
+
+<script type="text/ecmascript-6">
+import Header from './components/Header'
+import List from './components/List'
+import Footer from './components/Footer'
+  export default {
+    data(){
+      return {
+        todos:[
+          {id:1,name:'A',complated:false},
+          {id:2,name:'B',complated:true},
+          {id:3,name:'C',complated:false},
+          {id:4,name:'D',complated:true},
+        ]
+      }
+      
+    },
+  
+  components:{
+    Header,
+    List,
+    Footer
+  }
+  }
+</script>
+
+<style scoped>
+  .todo-container {
+    width: 600px;
+    margin: 0 auto;
+  }
+  .todo-container .todo-wrap {
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+  }
+</style>
