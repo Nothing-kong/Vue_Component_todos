4357eb750be340896060ffb103cba6cb322163c1
KLong <15110232612@163.com>
2019-12-07 13:43:51 +0800

    eventBus

---
+++ custom/event-bus.js
@@ -0,0 +1,43 @@
+/*
+自定义全局事件总线对象模块
+*/
+
+
+(function(window){
+  const eventBus = {}
+
+  let callBacksObj = {}
+
+  //绑定事件监听
+  eventBus.on = function (eventName,callback) {
+    const callbacks = callbacksObj[eventName]
+      if (callbacks) {
+        callbacks.push(callback)
+      } else {
+        callbacksObj[eventName] = [callback]
+      }
+  }
+  /* 
+    分发事件
+    */
+  eventBus.emit = function (eventName, data) {
+    const callbacks = callbacksObj[eventName]
+    if (callbacks && callbacks.length>0) {
+      callbacks.forEach(callback => {
+        callback(data)
+      })
+    }
+  }
+
+  /* 
+  移除事件监听
+  */
+  eventBus.off = function (eventName) {
+    if (eventName) {
+      delete callbacksObj[eventName]
+    } else {
+      callbacksObj = {}
+    }
+  }
+  
+})(window)
