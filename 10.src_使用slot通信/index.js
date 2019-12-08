df6b2894d81a5b0fc5629bc55187a7fcea26aade
KLong <15110232612@163.com>
2019-12-08 13:29:59 +0800

    footer

---
+++ 10.src_使用slot通信/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import App from './App'//引入自定义组件
+
+
+//注册全局组件
+//Vue.component('App',App)
+
+Vue.config.productionTip=false
+
+// Vue.prototype.globalEventBus = new Vue()
+
+new Vue({
+
+  beforeCreate() {
+    Vue.prototype.globalEventBus = this
+  },
+  
+   el:'#root',
+    
+   //注册局部组件
+   components:{
+     App:App
+   },
+   template:'<App/>'
+})
